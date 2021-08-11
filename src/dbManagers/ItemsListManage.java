c408c5a22120bf8a93e67c293c037d94e2a0dd6d
PedroProcter <OProcter89@gmail.com>
2021-08-11 08:42:00 -0400

    Refactor ItemsListManager.java.
    -fix the name of the package
    
    -fix the name of the interface implemented in line 20
    
    -fix name of the attribute (ArrayList) in line 31
    
    -fix the name of the column in line 60
    
    -fix the name of the var in line 66

--- src/dbManagers/ItemsListManage.java
+++ src/dbManagers/ItemsListManage.java
@@ -1,4 +1,8 @@
+<<<<<<< Updated upstream
 package dbManager;
+=======
+package dbManagers;
+>>>>>>> Stashed changes
 
 import java.sql.Statement;
 import java.sql.Connection;
@@ -6,16 +10,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+<<<<<<< Updated upstream
 import dataclasses.Client;
 
 public class ItemsListManage implements CanGetConnection {
+=======
+import dataclasses.PawnedItem;
+
+public class ItemsListManage implements CanGetDBConnection {
+>>>>>>> Stashed changes
     /*
     This class provides method to manage an ArrayList of clients
     
     */
     
     private Connection dbConnecion;
+<<<<<<< Updated upstream
     private ArrayList<PawnedItem> clients = new ArrayList<PawnedItem>();
+=======
+    private ArrayList<PawnedItem> items = new ArrayList<PawnedItem>();
+>>>>>>> Stashed changes
     
     @Override
     public void setDBConnection(Connection dbConnection){
@@ -35,12 +49,21 @@ public class ItemsListManage implements CanGetConnection {
                 
                 PawnedItem item = new PawnedItem();
                 
+<<<<<<< Updated upstream
                 item.setItemID(resultSet.getString("client_id"));
                 item.setItemName(resultSet.getString("name"));
                 item.setITemEstimateValue(resultSet.getInt("price"));
                 item.setItemDescription(resultSet.getString("description"));
                 
                 item.add(PawnedItem);
+=======
+                item.setItemID(resultSet.getString("item_id"));
+                item.setItemName(resultSet.getString("name"));
+                item.setItemEstimateValue(resultSet.getDouble("price"));
+                item.setItemDescription(resultSet.getString("description"));
+                
+                items.add(item);
+>>>>>>> Stashed changes
                 
                 
             }
